e9a7f3dde1410dd1abb680d3622a0f942b9bacb6
or roob <orroob28@gmail.com>
2021-10-31 10:23:03 +0200

    son is done

--- Son/main.c
+++ Son/main.c
@@ -83,29 +83,3 @@ int main(int argc, char* argv[])
 	}
 	return(0);
 }
-//HANDLE *hfile, char *fileName, DWORD accessMode, DWORD shareMode, DWORD creationDisposition, DWORD flagsAndAttributes
-
-
-	/*if (openFile(&hfile, fName, READ))
-	{
-		return(1);
-	}
-
-	if (WriteToFile(hfile, fName, strlen(fName)))
-	{
-		closeFile(&hfile);
-		return(1);
-	}
-	if (ReadFromFile(hfile, strTST, 20))
-	{
-		closeFile(&hfile);
-		return(1);
-	}
-
-	if (closeFile(&hfile))
-	{
-		return(1);
-	}
-	else
-		return(0);
-	*/
